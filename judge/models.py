6573e3161ab1cd91973d5599392502c309362750
Lipika-Rajpal <76445343+Lipika-Rajpal@users.noreply.github.com>
2022-07-08 19:09:34 +0530

    Update models.py

--- judge/models.py
+++ judge/models.py
@@ -12,13 +12,13 @@ class Problems(models.Model):
 class Solutions(models.Model):
     verdict = models.CharField(max_length = 200)
     submitted_at = models.DateTimeField(max_length=200)
-    problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problems, related_name= "sol", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.verdict
 
 class TestCases(models.Model):
-    problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problems, related_name= "tc",  on_delete=models.CASCADE)
     input = models.CharField(max_length=255)
     output = models.CharField(max_length=255)
 
