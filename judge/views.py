48f3a3e56666ecee919a6e6966bcdd93331924c9
Lipika-Rajpal <76445343+Lipika-Rajpal@users.noreply.github.com>
2022-07-08 19:11:07 +0530

    Update views.py

--- judge/views.py
+++ judge/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from judge.models import  Problems, Solutions
+import os, filecmp
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("hi")
+def problems(request):
+    problems_list = Problems.objects.all()
+    context = {"Problems_list": problems_list}
+    return render(request, "problems_list.html", context)
+
+def problem_detail(request, problem_id):
+    problem = get_object_or_404(Problems, id = problem_id)
+    return render(request, 'problem_detail.html', {'problem': problem})
+
+
+
+
