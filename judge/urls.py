3784d0b5702b1dd22c30dd86212fd71afc6f4e0b
Lipika-Rajpal <76445343+Lipika-Rajpal@users.noreply.github.com>
2022-07-08 19:10:20 +0530

    Update urls.py

--- judge/urls.py
+++ judge/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from judge import views
-
+app_name = 'judge'
 urlpatterns = [
 
-    path('judge/', views.index, name = 'problem')
+    path('', views.problems, name = 'problem'),
+    path('problems/<int:problem_id>/',views.problem_detail, name = 'problem_detail'  )
 ]
